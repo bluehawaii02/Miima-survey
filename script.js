1977e724a97f07b2fbab28570d8578128afbfb7b
Torongei <daltonkimutai02@gmail.com>
2025-09-11 01:40:22 +0300

    just made the quote page though not done

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const dropdown = document.querySelector(".bx-menu");
-const dropdownContent = document.querySelector(".head-mid-dropdown");
+const dropdownContent = document.querySelector(".head-mid-nd");
+const dropdownLost = document.querySelector(".head-mid-st");
+const getQuote = document.querySelector(".button-quote");
+const getQuote2 = document.querySelector(".get-started");
 
-dropdown.addEventListener("click", () => {
-  dropdownContent.style.display = "block";
-  dropdown.style.color = "red";
-});
+if(dropdown && dropdownContent &&dropdownLost){
+  dropdown.addEventListener("click", () => {
+    dropdownContent.style.display = "flex";
+    dropdownContent.style.flexDirection = "column";
+    dropdownLost.style.display = "none";
+  });
+}
 
-/*dropdown.addEventListener("mouseleave", () => {
-    dropdownContent.style.display = "none";
-})*/
+if(getQuote && getQuote2){
+  getQuote.addEventListener("click", () => {
+    window.location = "quote.html";
+  });
+
+  getQuote2.addEventListener("click", () => {
+    window.location = "quote.html";
+  });
+}
