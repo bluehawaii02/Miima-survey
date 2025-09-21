2716097c5eb760090c791043c0e85a2638d2cdbe
Torongei <daltonkimutai02@gmail.com>
2025-09-21 10:18:17 +0300

    project is almost done

--- quote.js
+++ quote.js
@@ -10,3 +10,46 @@ if(dropBtn && dropContent){
         };
     })
 };
+
+//let's add interactivity to the Go To payment button
+
+const GTpaymentBtn = document.querySelector(".p-payment");
+const form = document.querySelector(".form02");
+const paymentPage = document.querySelector(".payment");
+const Ximg = document.querySelector(".img5");
+
+if(GTpaymentBtn){
+    GTpaymentBtn.addEventListener("click", () => {
+        form.style.display = "none";
+        paymentPage.style.display = "flex";
+        Ximg.style.display = "none";
+    });
+};
+
+const ToZelle = document.querySelector(".zelleBtn");
+const ToBank = document.querySelector(".bankBtn");
+const ToCard = document.querySelector(".creditBtn");
+const ZellePage =  document.querySelector(".zelle");
+const BankPage = document.querySelector(".bank");
+
+if(ToZelle && ZellePage && BankPage){
+    ToZelle.addEventListener("click", () => {
+        if(ZellePage.style.display == "none"){
+        ZellePage.style.display = "flex";
+        BankPage.style.display = "none";
+        }else{
+            ZellePage.style.display = "none";
+        };
+    });
+}
+
+if(ToBank && BankPage && ZellePage){
+    ToBank.addEventListener("click", () => {
+        if(BankPage.style.display == "none"){
+            BankPage.style.display = "flex";
+            ZellePage.style.display = "none";
+        }else{
+            BankPage.style.display = "none";
+        };
+    });
+}
