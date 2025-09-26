f72f666db619f60ef9c673ca96d13823fc85d398
Torongei <daltonkimutai02@gmail.com>
2025-09-26 12:49:41 +0300

    listening to music

--- servicesChildren/service.js
+++ servicesChildren/service.js
@@ -1,1 +1,34 @@
-const QuoteBBtn = document.querySelector(".G")
+const DropBtn = document.querySelector(".third-a");
+const DropContentTorongei =  document.querySelector(".service-child");
+const DropIcon = document.querySelector(".bi-caret-down");
+
+
+if(DropBtn && DropContentTorongei && DropIcon){
+    DropBtn.addEventListener("click", () => {
+        if(getComputedStyle(DropContentTorongei).display === "none"){
+            DropContentTorongei.style.display = "flex"
+            DropIcon.classList.add("bi-caret-up");
+            window.alert("clicked")
+        }else{
+            DropContentTorongei.style.display = "none";
+            DropIcon.classlist = "bi-caret-down";
+        };
+    })
+}
+
+
+//let's add interactivity to the menu icon
+
+const menuBtnServ = document.querySelector(".bi-list");
+const navBarSide =  document.querySelector(".head-mid-nd");
+
+
+if(menuBtnServ && navBarSide){
+    menuBtnServ.addEventListener("click", () => {
+        if(getComputedStyle(navBarSide).display === "none"){
+            navBarSide.style.display = "flex";
+        }else{
+            navBarSide.style.display = "none";
+        }
+    })
+}
